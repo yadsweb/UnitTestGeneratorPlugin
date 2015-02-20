8d6d169c84e455f5a8ae4cafb7e2be656e49f7f8
Yani Dimitrov Stoyanov <yads.web@gmail.com>
2015-02-20 13:45:21 +0200

    Adding a class to handle the configuration.

--- UnitTestGeneratorPlugin/CustomUnitTestFeatureGenerator.cs
+++ UnitTestGeneratorPlugin/CustomUnitTestFeatureGenerator.cs
@@ -2,7 +2,10 @@
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using TechTalk.SpecFlow.Generator;
 using TechTalk.SpecFlow.Generator.Configuration;
 using TechTalk.SpecFlow.Generator.UnitTestConverter;
@@ -29,7 +32,20 @@ namespace UnitTestGeneratorPlugin.Generator.SpecFlowPlugin
 
         public new CodeNamespace GenerateUnitTestFixture(Feature feature, string testClassName, string targetNamespace)
         {
-            if (ConfigurationManager.AppSettings["Execution.type"] == null)
+
+            //var text = new StringBuilder();
+            //text.Append(" directory: " + Directory.GetCurrentDirectory());
+
+            var fileMap = new ExeConfigurationFileMap
+            {
+                ExeConfigFilename = Directory.GetCurrentDirectory() + @"\App.config"
+            };
+
+            var appConfig = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+
+            //text.Append(" Custom.plugin.generator.configuration value: " + appConfig.AppSettings.Settings["Custom.plugin.generator.configuration"].Value);
+
+            if (appConfig.AppSettings.Settings["Custom.plugin.generator.configuration"] == null)
             {
                 foreach (var scenario in feature.Scenarios)
                 {
@@ -37,6 +53,46 @@ namespace UnitTestGeneratorPlugin.Generator.SpecFlowPlugin
                     scenario.Tags.Add(new Tag("uniqueId:" + GenerateuniqueId()));
                 }
             }
+            else
+            {
+                try
+                {
+                    //text.AppendLine(appConfig.Sections["GeneratorPluginConfiguration"].CurrentConfiguration.ToString());
+                    //text.AppendLine("--------===-----------");
+                    var additionalCategoryAttributes = GeneratorPluginConfiguration.GetConfig(appConfig);
+                    //text.AppendLine("-------------------------");
+                    var additionalCategoryAttributes1 = ((GeneratorPluginConfiguration)additionalCategoryAttributes).AdditionalCategoryAttributes;
+                    //text.AppendLine("++++++++++");
+                    //var additionalTestCaseAttributes =
+                    //    GeneratorPluginConfiguration.GetConfig(appConfig).AdditionalTestCaseAttributes;
+                    //var additionalStesps =
+                    //    GeneratorPluginConfiguration.GetConfig(appConfig).AdditionalCategoryAttributes;
+
+                    //foreach (var scenario in feature.Scenarios)
+                    //{
+                    //    scenario.Tags = scenario.Tags ?? new Tags();
+
+                    //    text.Append("AdditionalCategoryAttribute: " + additionalCategoryAttributes.Count);
+
+                    //    if (additionalCategoryAttributes.Count > 0)
+                    //    {
+                    //        for (int counter = 0; counter < additionalCategoryAttributes.Count; counter++)
+                    //        {
+                    //            scenario.Tags.Add(additionalCategoryAttributes[counter].Type.Contains("unique")
+                    //                ? new Tag(additionalCategoryAttributes[counter].Value + GenerateuniqueId())
+                    //                : new Tag(additionalCategoryAttributes[counter].Value));
+                    //        }
+                    //    }
+                    //}
+                }
+                catch (Exception e)
+                {
+
+                    //text.Append(e.ToString());
+                }
+            }
+           
+            //File.WriteAllText(@"C:\log.txt", text.ToString());
             return base.GenerateUnitTestFixture(feature, testClassName, targetNamespace);
         }
 
---
+++ UnitTestGeneratorPlugin/GeneratorPluginConfiguration.cs
@@ -0,0 +1,282 @@
+﻿using System.Configuration;
+
+namespace UnitTestGeneratorPlugin.Generator.SpecFlowPlugin
+{
+    public class GeneratorPluginConfiguration : ConfigurationSection
+    {
+        private static GeneratorPluginConfiguration _config;
+
+        public static GeneratorPluginConfiguration GetConfig()
+        {
+            if (_config == null)
+            {
+                _config = (GeneratorPluginConfiguration)ConfigurationManager.GetSection("GeneratorPluginConfiguration");
+            }
+            return _config;
+        }
+        public static ConfigurationSection GetConfig(Configuration configuration)
+        {
+            return configuration.GetSection("GeneratorPluginConfiguration");
+        }
+
+        [ConfigurationProperty("AdditionalCategoryAttributes")]
+        [ConfigurationCollection(typeof(AdditionalCategoryAttributes), AddItemName = "AdditionalCategoryAttribute")]
+        public AdditionalCategoryAttributes AdditionalCategoryAttributes
+        {
+            get
+            {
+                object o = this["AdditionalCategoryAttributes"];
+                return o as AdditionalCategoryAttributes;
+            }
+        }
+
+        [ConfigurationProperty("AdditionalTestCaseAttributes")]
+        [ConfigurationCollection(typeof(AdditionalTestCaseAttributes), AddItemName = "AdditionalTestCaseAttribute")]
+        public AdditionalTestCaseAttributes AdditionalTestCaseAttributes
+        {
+            get
+            {
+                object o = this["AdditionalTestCaseAttributes"];
+                return o as AdditionalTestCaseAttributes;
+            }
+        }
+
+        [ConfigurationProperty("Steps")]
+        [ConfigurationCollection(typeof(Steps), AddItemName = "Step")]
+        public Steps Steps
+        {
+            get
+            {
+                object o = this["Steps"];
+                return o as Steps;
+            }
+        }
+
+        [ConfigurationProperty("TestCaseAttributeFilter")]
+        public TestCaseAttributeFilter TestCaseAttributeFilter
+        {
+            get { return this["TestCaseAttributeFilter"] as TestCaseAttributeFilter; }
+        }
+
+    }
+
+    public class TestCaseAttributeFilter : ConfigurationElement
+    {
+
+        [ConfigurationProperty("classname", IsRequired = true)]
+        public string Classname
+        {
+            get
+            {
+                return this["classname"] as string;
+            }
+        }
+        [ConfigurationProperty("namespace", IsRequired = true)]
+        public string Namespace
+        {
+            get
+            {
+                return this["namespace"] as string;
+            }
+        }
+    }
+
+    public class AdditionalCategoryAttribute : ConfigurationElement
+    {
+
+        [ConfigurationProperty("type", IsRequired = true)]
+        public string Type
+        {
+            get
+            {
+                return this["type"] as string;
+            }
+        }
+        [ConfigurationProperty("value", IsRequired = true)]
+        public string Value
+        {
+            get
+            {
+                return this["value"] as string;
+            }
+        }
+    }
+
+    public class AdditionalCategoryAttributes : ConfigurationElementCollection
+    {
+        public AdditionalCategoryAttribute this[int index]
+        {
+            get
+            {
+                return BaseGet(index) as AdditionalCategoryAttribute;
+            }
+            set
+            {
+                if (BaseGet(index) != null)
+                {
+                    BaseRemoveAt(index);
+                }
+                BaseAdd(index, value);
+            }
+        }
+
+        public new AdditionalCategoryAttribute this[string responseString]
+        {
+            get { return (AdditionalCategoryAttribute)BaseGet(responseString); }
+            set
+            {
+                if (BaseGet(responseString) != null)
+                {
+                    BaseRemoveAt(BaseIndexOf(BaseGet(responseString)));
+                }
+                BaseAdd(value);
+            }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new AdditionalCategoryAttribute();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((AdditionalCategoryAttribute)element).Value;
+        }
+    }
+
+    public class AdditionalTestCaseAttribute : ConfigurationElement
+    {
+
+        [ConfigurationProperty("type", IsRequired = true)]
+        public string Type
+        {
+            get
+            {
+                return this["type"] as string;
+            }
+        }
+        [ConfigurationProperty("value", IsRequired = true)]
+        public string Value
+        {
+            get
+            {
+                return this["value"] as string;
+            }
+        }
+    }
+
+    public class AdditionalTestCaseAttributes : ConfigurationElementCollection
+    {
+        public AdditionalTestCaseAttribute this[int index]
+        {
+            get
+            {
+                return BaseGet(index) as AdditionalTestCaseAttribute;
+            }
+            set
+            {
+                if (BaseGet(index) != null)
+                {
+                    BaseRemoveAt(index);
+                }
+                BaseAdd(index, value);
+            }
+        }
+
+        public new AdditionalTestCaseAttribute this[string responseString]
+        {
+            get { return (AdditionalTestCaseAttribute)BaseGet(responseString); }
+            set
+            {
+                if (BaseGet(responseString) != null)
+                {
+                    BaseRemoveAt(BaseIndexOf(BaseGet(responseString)));
+                }
+                BaseAdd(value);
+            }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new AdditionalTestCaseAttribute();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((AdditionalTestCaseAttribute)element).Value;
+        }
+    }
+
+    public class Step : ConfigurationElement
+    {
+
+        [ConfigurationProperty("type", IsRequired = true)]
+        public string Type
+        {
+            get
+            {
+                return this["type"] as string;
+            }
+        }
+        [ConfigurationProperty("value", IsRequired = true)]
+        public string Value
+        {
+            get
+            {
+                return this["value"] as string;
+            }
+        }
+        [ConfigurationProperty("position", IsRequired = true)]
+        public string Position
+        {
+            get
+            {
+                return this["position"] as string;
+            }
+        }
+    }
+
+    public class Steps : ConfigurationElementCollection
+    {
+        public Step this[int index]
+        {
+            get
+            {
+                return BaseGet(index) as Step;
+            }
+            set
+            {
+                if (BaseGet(index) != null)
+                {
+                    BaseRemoveAt(index);
+                }
+                BaseAdd(index, value);
+            }
+        }
+
+        public new Step this[string responseString]
+        {
+            get { return (Step)BaseGet(responseString); }
+            set
+            {
+                if (BaseGet(responseString) != null)
+                {
+                    BaseRemoveAt(BaseIndexOf(BaseGet(responseString)));
+                }
+                BaseAdd(value);
+            }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new Step();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((Step)element).Position;
+        }
+    }
+
+}
+
